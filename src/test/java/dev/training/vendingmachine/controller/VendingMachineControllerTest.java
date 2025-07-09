ced2b3b09a982ea47bb77ac91bd7d850b2fb38df
Toshiyuki Ando <920978+nobiinu-and@users.noreply.github.com>
2025-07-09 10:47:56 +0000

    Change: テストケースで期待する入力値をより明確に表現するように

--- src/test/java/dev/training/vendingmachine/controller/VendingMachineControllerTest.java
+++ src/test/java/dev/training/vendingmachine/controller/VendingMachineControllerTest.java
@@ -1,8 +1,5 @@
 package dev.training.vendingmachine.controller;
 
-import dev.training.vendingmachine.dto.PurchaseRequest;
-import dev.training.vendingmachine.dto.PurchaseResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,16 +15,11 @@ class VendingMachineControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
     @Test
     void _100円でコーラを購入できる() throws Exception {
-        PurchaseRequest request = new PurchaseRequest(100);
-        
         mockMvc.perform(post("/api/purchase")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(request)))
+                .content("{\"money\": 100}"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.product").value("コーラ"))
                 .andExpect(jsonPath("$.quantity").value(1));
